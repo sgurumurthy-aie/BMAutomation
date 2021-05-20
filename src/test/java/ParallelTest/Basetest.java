01881ada5ac51c37633e67ad8ff74ba6e171f222
sgurumurthy-aie <sgurumurthy@aienterprise.com>
2021-05-20 13:08:50 +0530

    changes are done incluse driverfactory

--- src/test/java/ParallelTest/Basetest.java
+++ src/test/java/ParallelTest/Basetest.java
@@ -13,7 +13,7 @@ import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.markuputils.ExtentColor;
 import com.aventstack.extentreports.markuputils.Markup;
 import com.aventstack.extentreports.markuputils.MarkupHelper;
-import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -38,58 +38,58 @@ public class Basetest {
 
 	}
 	
-	public ExtentHtmlReporter htmlReporter;
-	public ExtentTest test;
-	public ExtentReports extent;
-
-	
-	@BeforeTest
-	public void reportsetup() {
-	
-		htmlReporter = new ExtentHtmlReporter("./extentreports/extents.html");
-		
-		htmlReporter.config().setEncoding("utf-8");
-		htmlReporter.config().setDocumentTitle("BM Execution Report");
-		htmlReporter.config().setReportName("Production Report");
-		htmlReporter.config().setTheme(Theme.STANDARD);
-		htmlReporter.config().setTimeStampFormat("DD:MM:YYY");
-		
-		
-		extent = new ExtentReports();
-		extent.attachReporter(htmlReporter);
-		
-		extent.setSystemInfo("Shashikumar", "BMUSA");
-		extent.setSystemInfo("Organization", "AIE");
-		extent.setSystemInfo("Production application", "Test Result");
-		
-			
-	}
-	
-	
-	@AfterTest
-	public void endReport() {
-		extent.flush();
-		
-	}
-	
-	@AfterMethod
-	
-	public void teardown(ITestResult result) {
-		
-		if (result.getStatus() == ITestResult.FAILURE) {
-			
-		
-	}else if (result.getStatus() == ITestResult.SUCCESS) {
-		
-		String methodName = result.getMethod().getMethodName();
-		
-		String LogText = "<b>" + "Testcase: - "+methodName.toUpperCase() + "   PASSED " +"</b>";
-		Markup mark = MarkupHelper.createLabel(LogText, ExtentColor.GREEN);
-		
-		test.pass(mark);
-	}
-		
-	}
+//	public ExtentSparkReporter htmlReporter;
+//	public ExtentTest test;
+//	public ExtentReports extent;
+//
+//	
+//	@BeforeTest
+//	public void reportsetup() {
+//	
+//		htmlReporter = new ExtentSparkReporter("./extentreports/extents.html");
+//		
+//		htmlReporter.config().setEncoding("utf-8");
+//		htmlReporter.config().setDocumentTitle("BM Execution Report");
+//		htmlReporter.config().setReportName("Production Report");
+//		htmlReporter.config().setTheme(Theme.STANDARD);
+//		htmlReporter.config().setTimeStampFormat("DD:MM:YYY");
+//		
+//		
+//		extent = new ExtentReports();
+//		extent.attachReporter(htmlReporter);
+//		
+//		extent.setSystemInfo("Shashikumar", "BMUSA");
+//		extent.setSystemInfo("Organization", "AIE");
+//		extent.setSystemInfo("Production application", "Test Result");
+//		
+//			
+//	}
+//	
+//	
+//	@AfterTest
+//	public void endReport() {
+//		extent.flush();
+//		
+//	}
+//	
+//	@AfterMethod
+//	
+//	public void teardown(ITestResult result) {
+//		
+//		if (result.getStatus() == ITestResult.FAILURE) {
+//			
+//		
+//	}else if (result.getStatus() == ITestResult.SUCCESS) {
+//		
+//		String methodName = result.getMethod().getMethodName();
+//		
+//		String LogText = "<b>" + "Testcase: - "+methodName.toUpperCase() + "   PASSED " +"</b>";
+//		Markup mark = MarkupHelper.createLabel(LogText, ExtentColor.GREEN);
+//		
+//		test.pass(mark);
+//	}
+//		
+//	}
 
 
 }
--- src/test/java/ParallelTest/ParallelExecution.java
+++ src/test/java/ParallelTest/ParallelExecution.java
@@ -1,30 +1,49 @@
 package ParallelTest;
 
+import java.net.MalformedURLException;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
-public class ParallelExecution extends Basetest{
+public class ParallelExecution {
 	
 	public static WebDriver webDriver;
 	
-//	@Parameters({"Browsers"})
-//	@Test
-//	public void displayBrowser(String browsers) {
-//		
-//		System.out.println("Browser"+"---"+ browsers);
-//	
-//	}
+	BrowserFactory bfs = new BrowserFactory();
+	SoftAssert softassert = new SoftAssert();
+	
+
+	
+	
+	
+	@Parameters ({"Browsers"})
+	@BeforeMethod
+	public void browserlaunch(String Browsers ) throws MalformedURLException {
+
+	DriverFactory.getInstance().setDriver(bfs.createBrowserInstance(Browsers));
+	DriverFactory.getInstance().getDriver().navigate().to("https://www.brandsmartusa.com");	
+	DriverFactory.getInstance().getDriver().manage().window().maximize();
+	
+
+	}
+	@AfterMethod
+	public void closebrowser() {
+	DriverFactory.getInstance().closeBrowser();	
+	}
 	
 	@Test
-	public void browserlaunch() {
-
-	webDriver = getdriver("chrome");
-	webDriver.get("https://www.brandsmartusa.com");	
-	webDriver.findElement(By.xpath("//*[@id=\"options\"]/li[1]/div/ul/li[2]/a[1]")).click();;
-	
-	
-	
+	public void onClickLoginlink() {
+		try {
+		DriverFactory.getInstance().getDriver().findElement(By.xpath("//*[@id=\"options\"]/li[1]/div/ul/li[2]/a[1]")).click();
+		ExtentFactory.getInstance().getExtent().info("sucessful link click");
+		} catch (Exception e) {
+			ExtentFactory.getInstance().getExtent().info("unsucessful link click");
+		}
 	}
 	
 	
